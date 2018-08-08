34d599e9242607faf151c92d289bbe443ebc0569
UmaHani <umahanimohammadi@yahoo.com>
2018-08-08 15:21:08 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -8,8 +8,7 @@ STUDENT_NAMES = [
 ]
 
 def first_student_by_index
-  # Write a solution that returns the first student in the array student_names
-  puts first_student_by_index 
+  # first_student_by_index 
 end
 
 def fourth_student_by_index
